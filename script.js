8e3cc57c930101cb0af8d8386a9bcfe1c7a8eadc
Joel Anang <133300552+Jo-erl@users.noreply.github.com>
2023-06-11 18:46:47 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
+
+// Function to get the current date in the desired format
+function getCurrentDate() {
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const currentDate = new Date();
+  const day = days[currentDate.getDay()];
+  const month = months[currentDate.getMonth()];
+  const date = currentDate.getDate();
+  const year = currentDate.getFullYear();
+
+  return `${day}, ${month} ${date}, ${year}`;
+}
+
+// Update the current date element
+document.getElementById('currentDate').textContent = getCurrentDate();
