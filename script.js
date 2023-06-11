a269cbf4aa135420c9fb475fa17bc62d8d48f3d9
Joel Anang <133300552+Jo-erl@users.noreply.github.com>
2023-06-11 19:26:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,29 @@ setInterval(() => {
 
 // Function to get the current date in the desired format
 function getCurrentDate() {
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
   const currentDate = new Date();
   const day = days[currentDate.getDay()];
@@ -27,4 +48,46 @@ function getCurrentDate() {
 }
 
 // Update the current date element
-document.getElementById('currentDate').textContent = getCurrentDate();
+document.getElementById("currentDate").textContent = getCurrentDate();
+
+function updateClock() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  var ampm = hours >= 12 ? "PM" : "AM";
+
+  // Convert hours to 12-hour format
+  hours = hours % 12 || 12;
+
+  // Format hours, minutes, and seconds to have leading zeros if needed
+  hours = hours.toString().padStart(2, "0");
+  minutes = minutes.toString().padStart(2, "0");
+  seconds = seconds.toString().padStart(2, "0");
+
+  // Display the current time in the format 00:00:00 AM/PM
+  var currentTimeString = hours + ":" + minutes + ":" + seconds + " " + ampm;
+  document.getElementById("currentTime").textContent = currentTimeString;
+
+  // Check for holidays
+  var holidays = ["2023-07-04", "2023-12-25"]; // Example holidays
+  var currentDate = new Date().toISOString().slice(0, 10);
+  if (holidays.includes(currentDate)) {
+    document.getElementById("holidayMessage").textContent =
+      "Today is a holiday!";
+  } else {
+    document.getElementById("holidayMessage").textContent = "";
+  }
+}
+/**Check for holidays
+  var holidays = ['2023-07-04', '2023-12-25']; // Example holidays
+  var currentDate = new Date().toISOString().slice(0, 10);
+  if (holidays.includes(currentDate)) {
+    document.getElementById('holidayMessage').textContent = 'Today is a holiday!';
+  } else {
+    document.getElementById('holidayMessage').textContent = '';
+  }
+} */
+
+// Update the clock every second
+setInterval(updateClock, 1000);
